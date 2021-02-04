69ae261ef10e3cac1267376ca4f2d20aa6463c01
Shamil <badalloffsh@gmail.com>
2021-02-04 23:35:21 +0100

    Add: link to main page

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SideMenu = ({ isLoading, chapters }) => {
   return (
     <div className="side-menu-wrapper">
       <div className="side-menu">
-        <h1 className="header-logo">booksLie</h1>
+        <Link to="/">
+          <h1 className="header-logo">booksLie</h1>
+        </Link>
 
         <div className="loading-status">{isLoading && "Loading ... "}</div>
 
         <ul className="navigation">
+          <li key="mainPage" className="navigation-item">
+            <NavLink to={`/`} activeClassName="selected" exact>
+              Main Page
+            </NavLink>
+          </li>
           {chapters &&
             chapters.map((chapter) => {
               return (
