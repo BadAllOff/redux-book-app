53348f6392af1769f217cf9a2df8318ed4da554e
Shamil <badalloffsh@gmail.com>
2021-02-04 01:22:04 +0100

    Add: portal modal component for chapter edit action

--- src/components/ContentList/Chapter/Chapter.js
+++ src/components/ContentList/Chapter/Chapter.js
@@ -1,33 +1,38 @@
-import React, { useState } from "react";
-import { Modal } from "react-bootstrap";
+import React, { useRef } from "react";
+import MyModal from "../../Modal/Modal";
 import SubsectionList from "./SubsectionList/";
 import { Link } from "react-router-dom";
 
 const Chapter = ({ chapter, toggleReady, deleteChapter, editChapter }) => {
+  const modal = useRef(null);
+
   return (
     <li>
       <h4>{chapter.title}</h4>
       <Link to={`/chapters/${chapter._id}`}>preview</Link>
       <button
-        class="btn btn-outline-danger btn-sm"
+        className="btn btn-outline-danger btn-sm"
         onClick={() => deleteChapter(chapter._id)}
       >
         Delete chapter
       </button>
-      <ModalEdit chapter={chapter} editChapter={editChapter}></ModalEdit>
-      <div class="form-group">
-        <div class="form-check">
+      <button onClick={() => modal.current.open()}>Open Modal</button>
+      <MyModal ref={modal}>
+        <ModalEdit chapter={chapter} editChapter={editChapter}></ModalEdit>
+      </MyModal>
+      <div className="form-group">
+        <div className="form-check">
           <input
             onChange={() => toggleReady(chapter._id)}
             name="ready"
             type="checkbox"
             id={["ready", chapter._id].join("_")}
-            class="form-check-input"
+            className="form-check-input"
           />
           <label
             title=""
-            for={["ready", chapter._id].join("_")}
-            class="form-check-label"
+            htmlFor={["ready", chapter._id].join("_")}
+            className="form-check-label"
           >
             Mark as ready
           </label>
@@ -43,59 +48,35 @@ const Chapter = ({ chapter, toggleReady, deleteChapter, editChapter }) => {
 export default Chapter;
 
 const ModalEdit = ({ chapter, editChapter }) => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
-    <>
-      <button className="btn btn-outline-success btn-sm" onClick={handleShow}>
-        Edit chapter
-      </button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit chapter </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <fieldset className="border p-2">
-            <legend className="w-auto">Add new chapter</legend>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                editChapter(chapter._id, { title: e.target.title.value });
-                e.target.title.value = "";
-              }}
-            >
-              <div>
-                <div>
-                  <div className="form-group">
-                    <label className="form-label">Edit Chapter</label>
-                    <input
-                      name="title"
-                      type="text"
-                      className="form-control form-control-lg"
-                      value={chapter.title}
-                    ></input>
-                    <small className="text-muted form-text">
-                      enter the new title of chapter
-                    </small>
-                  </div>
-                </div>
-              </div>
-              <button type="submit" class="btn btn-primary">
-                Save Changes
-              </button>
-            </form>
-          </fieldset>
-        </Modal.Body>
-        <Modal.Footer>
-          <button type="button" class="btn btn-secondary">
-            Close
-          </button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <fieldset>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          editChapter(chapter._id, { title: e.target.title.value });
+          e.target.title.value = "";
+        }}
+      >
+        <div>
+          <div>
+            <div className="form-group">
+              <label className="form-label">Write in new title for chapter</label>
+              <input
+                name="title"
+                type="text"
+                className="form-control form-control-lg"
+                defaultValue={chapter.title}
+              />
+              <small className="text-muted form-text">
+                enter the new title of chapter
+              </small>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Save Changes
+        </button>
+      </form>
+    </fieldset>
   );
 };
